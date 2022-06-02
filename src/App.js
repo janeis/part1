1ac3664ce7d3299d766816699a4068561e19717b
jane <1420130551@qq.com>
2022-06-02 10:46:46 +0800

    part1 one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Header=(props)=>{
+   return(
+     <div>
+       <p>
+        {props.part1} {props.part2} {props.part3}
+       </p>
+     </div>
+   )
+}
+const Content=(props)=> {
+  return(
+    <div>
+      <p>
+        {props.exercises1}
+        {props.exercises2}
+        {props.exercises3}
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+const Total=(props)=>{
+  return(
+    <div>
+      <p>{props.total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  
+  return (
+    <div>
+      <h1>{course}</h1>
+      课程名称
+     <Header part1= 'Fundamentals of React'/>
+     <Header part2='Using props to pass data' />
+     <Header part3='State of a component' />
+     课程章节
+     <Content exercises1='10' />
+     <Content exercises2='7' />
+     <Content exercises1='14' />
+     总数  <Total total={10+7+14} />
+ 
+    </div>
+  )
+  }
+
+export default App
